af9e20bf858d17fcfc380e6b1f7d8958b85523cb
José Antonio Burgos Francisco <65372353+JoseBurg@users.noreply.github.com>
2023-03-11 11:42:10 -0400

    Comentarios

--- Mastering Shiny/Empezando.R
+++ Mastering Shiny/Empezando.R
@@ -1,1 +1,12 @@
+
+# Paquetes necesario ------------------------------------------------------
+# 
+# install.packages(c(
+#   "gapminder", "ggforce", "gh", "globals", "openintro", "profvis", 
+#   "RSQLite", "shiny", "shinycssloaders", "shinyFeedback", 
+#   "shinythemes", "testthat", "thematic", "tidyverse", "vroom", 
+#   "waiter", "xml2", "zeallot" 
+# ))
+
+
 library(shiny)
