6ef5fdbf7767208497f5fd6bc88896746046cc07
José Antonio Burgos Francisco <65372353+JoseBurg@users.noreply.github.com>
2024-01-04 10:31:23 -0400

    Create forecasting_contin.R

---
+++ forecasting_contin.R
@@ -0,0 +1,4 @@
+## Pasos básicos para iniciar a pronosticar
+
+### 1. Definición del problema.
+Se debe conocer para que se requiere y que papel desempeñará dentro de la organización el pronóstico. El pronósticador debe de dedicar tiempo a las necesidades y problemas que enfrentarán sus pronósticos.
